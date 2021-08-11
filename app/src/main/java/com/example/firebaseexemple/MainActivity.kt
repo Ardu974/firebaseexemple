8300fdb624a65d1e499777a0903a931a46b89e27
Ardu974 <arnaud.dumont@spirun.com>
2021-08-11 15:41:25 +0400

    3-Initialisation de l'interface

--- app/src/main/java/com/example/firebaseexemple/MainActivity.kt
+++ app/src/main/java/com/example/firebaseexemple/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.firebaseexemple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val ETemail = findViewById<EditText>(R.id.email)
+        val ETpassword = findViewById<EditText>(R.id.password)
+        val btConnexion = findViewById<Button>(R.id.btConnecter)
+
+        btConnexion.setOnClickListener{}
     }
 }
